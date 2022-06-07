f82cc7d8e29fa80e739791b056430ee6719f9aa5
utrodus <come.programmer@gmail.com>
2022-06-07 17:31:32 +0700

    fix: error ssl

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sleep/src/app.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'src/utils/http_overrides.dart';
+
+void main() async {
+  // Fix Error Http Request CERTIFICATE_VERIFY_FAILED
+  // This userful for error image network request
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
