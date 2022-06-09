dbebfb778cc3c31a8aae9eaa527556480c6f984a
utrodus <come.programmer@gmail.com>
2022-06-09 15:29:25 +0700

    fix: refactor detailviewmodel

--- lib/src/view_models/detail_view_model.dart
+++ lib/src/view_models/detail_view_model.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 import '../models/progress_bar_model.dart';
-import '../models/sleep_media_model.dart';
 
 class DetailViewModel {
   final progressNotifier = ValueNotifier<ProgressBarState>(
@@ -15,7 +14,6 @@ class DetailViewModel {
   final buttonNotifier = ValueNotifier<ButtonState>(
     ButtonState.paused,
   );
-
   static String url = '';
 
   late AudioPlayer _audioPlayer;
@@ -24,54 +22,57 @@ class DetailViewModel {
   }
 
   void _init() async {
-    _audioPlayer = AudioPlayer();
-    await _audioPlayer.setUrl(url);
+    try {
+      _audioPlayer = AudioPlayer();
+      await _audioPlayer.setUrl(url);
 
-    _audioPlayer.setVolume(1);
+      _audioPlayer.setVolume(1);
 
-    _audioPlayer.playerStateStream.listen((playerState) {
-      final isPlaying = playerState.playing;
-      final processingState = playerState.processingState;
+      _audioPlayer.playerStateStream.listen((playerState) {
+        final isPlaying = playerState.playing;
+        final processingState = playerState.processingState;
 
-      if (processingState == ProcessingState.buffering ||
-          processingState == ProcessingState.loading) {
-        buttonNotifier.value = ButtonState.loading;
-      } else if (!isPlaying) {
-        buttonNotifier.value = ButtonState.paused;
-      } else if (processingState != ProcessingState.completed) {
-        buttonNotifier.value = ButtonState.playing;
-      } else {
-        _audioPlayer.seek(Duration.zero);
-        _audioPlayer.pause();
-      }
-    });
+        if (processingState == ProcessingState.buffering ||
+            processingState == ProcessingState.loading) {
+          buttonNotifier.value = ButtonState.loading;
+        } else if (!isPlaying) {
+          buttonNotifier.value = ButtonState.paused;
+        } else if (processingState != ProcessingState.completed) {
+          buttonNotifier.value = ButtonState.playing;
+        } else {
+          _audioPlayer.seek(Duration.zero);
+          _audioPlayer.pause();
+        }
+      });
 
-    _audioPlayer.positionStream.listen((position) {
-      final oldState = progressNotifier.value;
-      progressNotifier.value = ProgressBarState(
-        current: position,
-        buffered: oldState.buffered,
-        total: oldState.total,
-      );
-    });
+      _audioPlayer.positionStream.listen((position) {
+        final oldState = progressNotifier.value;
+        progressNotifier.value = ProgressBarState(
+          current: position,
+          buffered: oldState.buffered,
+          total: oldState.total,
+        );
+      });
 
-    _audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
-      final oldState = progressNotifier.value;
-      progressNotifier.value = ProgressBarState(
-        current: oldState.current,
-        buffered: bufferedPosition,
-        total: oldState.total,
-      );
-    });
+      _audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
+        final oldState = progressNotifier.value;
+        progressNotifier.value = ProgressBarState(
+          current: oldState.current,
+          buffered: bufferedPosition,
+          total: oldState.total,
+        );
+      });
 
-    _audioPlayer.durationStream.listen((totalDuration) {
-      final oldState = progressNotifier.value;
-      progressNotifier.value = ProgressBarState(
-        current: oldState.current,
-        buffered: oldState.buffered,
-        total: totalDuration ?? Duration.zero,
-      );
-    });
+      _audioPlayer.durationStream.listen((totalDuration) {
+        final oldState = progressNotifier.value;
+        progressNotifier.value = ProgressBarState(
+          current: oldState.current,
+          buffered: oldState.buffered,
+          total: totalDuration ?? Duration.zero,
+        );
+      });
+      // ignore: empty_catches
+    } catch (e) {}
   }
 
   void play() {
@@ -87,15 +88,7 @@ class DetailViewModel {
   }
 
   void dispose() {
-    _audioPlayer.stop();
     _audioPlayer.dispose();
-  }
-
-  List<SleepMediaItem> items = [];
-
-  toJson() {
-    return items.map((item) {
-      return item.toJson();
-    }).toList();
+    _audioPlayer.stop();
   }
 }
