61b451ce30a448aad3023f20147ec96c4e9d0a1a
utrodus <come.programmer@gmail.com>
2022-06-09 18:21:23 +0700

    fix: load local data if category is 4

--- lib/src/view_models/home_view_model.dart
+++ lib/src/view_models/home_view_model.dart
@@ -1,6 +1,9 @@
 /// Load Dummy Media Data
 
+import 'dart:convert';
+
 import 'package:sleep/src/models/sleep_media_model.dart';
+import 'package:sleep/src/utils/prefs_data.dart';
 
 import '../models/sleep_media_source.dart';
 
@@ -8,6 +11,11 @@ Future<List<SleepMediaItem>> getMediaDatas({int? categoryId}) {
   return Future.delayed(const Duration(seconds: 1), () {
     List<SleepMediaItem> filteredMedia = [];
     if (categoryId != null) {
+      /// category 1 = Stories
+      /// category 2 = Music
+      /// category 3 = Anxious
+      /// category 4 = My
+      ///
       if (categoryId == 1) {
         filteredMedia = sleepMediaDataSource
             .where((item) => item.category!.id == 1)
@@ -31,16 +39,21 @@ Future<List<SleepMediaItem>> getMediaDatas({int? categoryId}) {
         filteredMedia.addAll(musicList);
         return filteredMedia;
       } else if (categoryId == 4) {
-        var storyList = sleepMediaDataSource
-            .where((item) => item.category!.id == 1)
-            .toList()
-            .getRange(0, 2);
-        var musicList = sleepMediaDataSource
-            .where((item) => item.category!.id == 2)
-            .toList()
-            .getRange(0, 2);
-        filteredMedia.addAll(storyList);
-        filteredMedia.addAll(musicList);
+        List<String> tempData = [];
+        var data = PrefsData.getData('datas');
+        data.then((value) {
+          /// if data not empty
+          if (value != null) {
+            if (value != []) {
+              tempData = value;
+              for (var data in tempData) {
+                /// assign items data
+                filteredMedia.add(SleepMediaItem.fromJson(json.decode(data)));
+              }
+            }
+          }
+        });
+
         return filteredMedia;
       }
     }
